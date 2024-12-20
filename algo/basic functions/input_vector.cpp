75bde191778f6f5ae77f2e87ce434fc6149fd049
aryanthepain <aryan.gupta@iitg.ac.in>
2024-12-20 23:05:44 +0530

    moved folder here

--- algo/basic functions/input_vector.cpp
+++ algo/basic functions/input_vector.cpp
@@ -2,11 +2,11 @@
 #include "basics.h"
 
 template <typename T>
-void input_vector(vector<T> v)
+void input_vector(vector<T> &v)
 {
     for (size_t i = 0; i < v.size(); i++)
     {
-        cin << v[i];
+        cin >> v[i];
     }
 
     return;
